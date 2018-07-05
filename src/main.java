6a23f69295d2f833dbf6f124bbfa40fb79fb65c1
Charly <charly.strn@gmail.com>
2018-07-05 14:58:13 +0200

    Début du main et appel de jeu

---
+++ src/main.java
@@ -0,0 +1,16 @@
+import model.Jeu;
+
+public class main {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Jeu jeu = null;
+		jeu.start();
+	}
+	
+	
+
+}
---
+++ src/model/Jeu.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Jeu { 
+ 
+public void start(){ 
+   
+} 
+ 
+public void stop(){ 
+   
+} 
+ 
+public void endLevel(){ 
+   
+} 
+ 
+public void listNiveaux(){ 
+   
+} 
+   
+} 
