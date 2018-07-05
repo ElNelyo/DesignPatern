773ea975e7bbdb0f0ae7b8a773916a1598bff1a5
Charly <charly.strn@gmail.com>
2018-07-05 15:06:02 +0200

    Travail avec yassine le bg sur JEU

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -1,9 +1,15 @@
 package model;
+import java.util.Scanner;
+
 
 public class Jeu { 
  
 public void start(){ 
-   
+	Scanner reader = new Scanner(System.in);  // Reading from System.in
+	System.out.println("Entrer votre nom");
+	String name = reader.next(); // Scans the next token of the input as an int.
+	reader.close();
+	System.out.println("Nom choisi : "+name);
 } 
  
 public void stop(){ 
