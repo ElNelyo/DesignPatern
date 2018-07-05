5741c255097fc3ddd3c752b2332e00e38dc70fd0
Charly <charly.strn@gmail.com>
2018-07-05 15:22:01 +0200

    Niveau de test crée

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -10,7 +10,11 @@ public void start(){
 	String name = reader.next(); 
 	System.out.println("Nom choisi : "+name);
 	*/
+	
 	Labyrinthe labyrinthe = new Labyrinthe(9,9);
+	Niveau niveau = new Niveau();
+	niveau.niveau_test();
+	
 	// List de niveau , 
 	// Matrice du labyrinthe : case d'entrée de sortie 
 	// Case recoit le niveau
---
+++ src/model/Niveau.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Niveau {
+	
+	private int difficulte = 0;
+	
+	public int getDifficulte() {
+		return difficulte;
+	}
+	
+	public int setDifficulte(int diff) {
+		difficulte = diff;
+		return diff;
+	}
+	public void niveau_test(){
+		
+	}
+}
