50a3e7c03593d99e4569108cf2f98c8f38f17337
Charly <charly.strn@gmail.com>
2018-07-05 15:15:38 +0200

    Création d'un labyrinthe d'une taille de 9x9 dans Jeu.start()

---
+++ src/model/Case.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Case {
+	
+	public Case(){
+		
+	}
+	
+	
+	public void playerInteraction(){
+		
+		
+	}
+	
+}
--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class Jeu { 
  
 public void start(){ 
-	Scanner reader = new Scanner(System.in);
+	/* Scanner reader = new Scanner(System.in);
 	System.out.println("Entrer votre nom");
 	String name = reader.next(); 
 	System.out.println("Nom choisi : "+name);
+	*/
+	Labyrinthe labyrinthe = new Labyrinthe(9,9);
+	// List de niveau , 
+	// Matrice du labyrinthe : case d'entrée de sortie 
+	// Case recoit le niveau
+	
 } 
  
 public void stop(){ 
